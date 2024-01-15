3a7c2c3922cd60af705c90cb32cb3d094f7ac104
Django149 <103638798+Django149@users.noreply.github.com>
2024-01-15 22:15:22 +0200

    Add operators and the functionality of evaluate expressions (without parentheses). Add more custom exceptions.

--- errors.py
+++ errors.py
@@ -11,3 +11,13 @@ class InsufficientOperandsError(Exception):
 class UnknownCharacterError(Exception):
     """Exception raised when an invalid character is encountered in the expression"""
     pass
+
+
+class InsufficientOperatorsError(Exception):
+    """Exception raised when there are not enough operands for an operation."""
+    pass
+
+
+class InvalidUseOfOperatorError(Exception):
+    """Exception raised for invalid use of an operator."""
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,163 @@
-from errors import InvalidNumberFormatError, InsufficientOperandsError, UnknownCharacterError
+from errors import InvalidNumberFormatError, InsufficientOperandsError, UnknownCharacterError, \
+    InsufficientOperatorsError, InvalidUseOfOperatorError
+from operators import BinaryOperator, RightUnaryOperator, LeftUnaryOperator, UnaryOperator
+from math_functions import *
+
+OPERATORS = {"+": BinaryOperator(1, add), "-": BinaryOperator(1, subtract), "*": BinaryOperator(2, multiply),
+             "/": BinaryOperator(2, divide), "^": BinaryOperator(3, power), "unaryMinus": LeftUnaryOperator(3, negate),
+             "%": BinaryOperator(4, modulus),
+             "@": BinaryOperator(5, average),
+             "$": BinaryOperator(5, maximum), "&": BinaryOperator(5, minimum), "~": LeftUnaryOperator(6, negate),
+             "!": RightUnaryOperator(6, factorial), "#": RightUnaryOperator(6, sum_of_digits),
+             "numberMinus": LeftUnaryOperator(10, negate)}
+
+
+def is_number(item: str | int | float) -> bool:
+    """
+    Checks if the item is a number (integer or float)
+    :param item: The item to check
+    :return: True if item is a number (integer or float), False otherwise
+    """
+    return isinstance(item, int) or isinstance(item, float)
+
+
+def is_operator(item: str | int | float) -> bool:
+    """
+    Checks if the item is an operator
+    :param item: The item to check
+    :return: True if item is an operator, False otherwise
+    """
+    return item in OPERATORS
+
+
+def is_unary_operator(item: str | int | float) -> bool:
+    """
+    Checks if the item is a unary operator
+    :param item: The item to check
+    :return: True if item is a unary operator, False otherwise
+    """
+    return is_operator(item) and isinstance(OPERATORS[item], UnaryOperator)
+
+
+def is_right_unary_operator(item: str | int | float) -> bool:
+    """
+    Checks if the item is a right unary operator
+    :param item: The item to check
+    :return: True if item is a right unary operator, False otherwise
+    """
+    return is_operator(item) and isinstance(OPERATORS[item], RightUnaryOperator)
+
+
+def is_left_unary_operator(item: str | int | float) -> bool:
+    """
+    Checks if the item is a left unary operator
+    :param item: The item to check
+    :return: True if item is a left unary operator, False otherwise
+    """
+    return is_operator(item) and isinstance(OPERATORS[item], LeftUnaryOperator)
+
+
+def is_binary_operator(item: str | int | float) -> bool:
+    """
+    Checks if the item is a binary operator
+    :param item: The item to check
+    :return: True if item is a binary operator, False otherwise
+    """
+    return is_operator(item) and isinstance(OPERATORS[item], BinaryOperator)
+
+
+def execute_operation(operand_stack: list[int | float], operator_stack: list[str]):
+    """
+    Executes one unary operation or one binary operation and insert the result into the operand_stack
+    :param operand_stack: list of operands
+    :param operator_stack: list of operators
+    :raises InsufficientOperandsError: If there are insufficient operands to perform a binary operation
+    :raises InvalidUseOfOperatorError: If an operator is used incorrectly
+    :raises InsufficientOperatorsError: If there are mismatched parentheses
+    """
+    operator = operator_stack.pop()
+    if operator == '(':
+        raise InsufficientOperatorsError("Mismatched parentheses")
+
+    func = OPERATORS[operator].get_function()
+    if is_unary_operator(operator):
+        if not operand_stack:
+            if is_right_unary_operator(operator):
+                raise InvalidUseOfOperatorError(f"Invalid use of {operator} operator")
+            if is_left_unary_operator(operator):
+                return
+
+        operand = operand_stack.pop()
+
+        try:
+            num = func(operand)
+        except (ValueError, TypeError) as e:
+            raise InvalidUseOfOperatorError(e)
+        if isinstance(num, float) and num.is_integer():
+            num = int(num)
+
+        operand_stack.append(num)
+
+    else:
+        if len(operand_stack) < 2:
+            if operator == "(":
+                raise InsufficientOperatorsError("Mismatched parentheses")
+            else:
+                raise InsufficientOperandsError(f"Not enough operands for binary operation ('{operator}')")
+
+        operand2 = operand_stack.pop()
+        operand1 = operand_stack.pop()
+
+        try:
+            num = func(operand1, operand2)
+        except ZeroDivisionError as e:
+            raise InvalidUseOfOperatorError(e)
+
+        if isinstance(num, float) and num.is_integer():
+            num = int(num)
+
+        operand_stack.append(num)
+
+
+def handle_operator(operator_stack: list[str], operand_stack: list[int | float], operator: str,
+                    previous: str | int | float):
+    """
+    Handles the current operator in the expression and updates the operator_stack
+    :param operand_stack: list of operands
+    :param operator_stack: list of operators
+    :param operator: The operator to process
+    :param previous: The last character or number handled
+    :raises InvalidUseOfOperatorError: If an operator is used incorrectly or if the execute_operation function raises
+    this exception
+    :raises InsufficientOperandsError: If the execute_operation function raises this exception
+    :raises InsufficientOperatorsError: If the execute_operation function raises this exception
+    """
+    if is_left_unary_operator(previous) and operator != '-':
+        raise InvalidUseOfOperatorError(f"Operator '{previous}' needs to be next to a number")
+
+    if operator == '-':
+        if previous is None:
+            operator = "unaryMinus"
+        elif not is_right_unary_operator(previous) and not is_number(previous):
+            if previous == "unaryMinus":
+                operator = "unaryMinus"
+            else:
+                operator = "numberMinus"
+
+    if is_right_unary_operator(operator) and not (is_number(previous) or is_right_unary_operator(previous)):
+        raise InvalidUseOfOperatorError(f"Operator '{operator}' should be to the right of a number")
+
+    if is_left_unary_operator(operator):
+        if is_number(previous) or is_right_unary_operator(previous):
+            raise InvalidUseOfOperatorError(f"Operator '{operator}' should be to the left of a number")
+        operator_stack.append(operator)
+        return
+
+    while operator_stack and operator_stack[-1] != '(' and OPERATORS[operator_stack[-1]].get_precedence() >= \
+            OPERATORS[operator].get_precedence():
+        execute_operation(operand_stack, operator_stack)
+
+    operator_stack.append(operator)
 
 
 def get_number(expression: str, index: int) -> tuple[int, int]:
@@ -37,15 +196,20 @@ def get_number(expression: str, index: int) -> tuple[int, int]:
     return number, index - 1
 
 
-def handle_number(operand_stack: list[int | float], expression: str, index: int) -> tuple[int, int]:
+def handle_number(operand_stack: list[int | float], expression: str, index: int, previous: str | int | float) -> tuple[
+    int, int]:
     """
     Finds the number in the index place in the expression and insert the result into the operand_stack
     :param operand_stack: list of operands
     :param expression: The expression to find the number in
     :param index: The index where the number starts
+    :param previous: The last character or number handled
     :return: A tuple containing the number and the index to proceed from
     :raises InvalidNumberFormatError: if the get_number function raises this exception
+    :raises InsufficientOperatorsError: if there are not enough operators for a binary operation
     """
+    if is_number(previous) or is_right_unary_operator(previous):
+        raise InsufficientOperatorsError("Not enough operators for a binary operation")
     number, index = get_number(expression, index)
     operand_stack.append(number)
     return number, index
@@ -58,10 +222,16 @@ def evaluate_expression(expression: str) -> int | float:
     :return: The result of the expression
     :raises InvalidNumberFormatError: If the handle_number function raises this exception
     :raises UnknownCharacterError: If an invalid character is encountered in the expression
-    :raises InsufficientOperandsError: If there are not enough operands in the expression
+    :raises InsufficientOperandsError: If there are not enough operands in the expression or if the handle_operator
+    function raises this exception
+    :raises InsufficientOperatorsError: If the handle_number function raises this exception or if the handle_operator
+    function raises this exception
+    :raises InvalidUseOfOperatorError: If the handle_operator function raises this exception
     """
     operand_stack = []
+    operator_stack = []
     index = 0
+    previous = None
     while index < len(expression):
         char = expression[index]
 
@@ -69,12 +239,20 @@ def evaluate_expression(expression: str) -> int | float:
             pass
 
         elif char.isdigit():
-            number, index = handle_number(operand_stack, expression, index)
+            number, index = handle_number(operand_stack, expression, index, previous)
+            previous = number
+
+        elif is_operator(char):
+            handle_operator(operator_stack, operand_stack, char, previous)
+            previous = operator_stack[-1]
 
         else:
             raise UnknownCharacterError(f"Invalid character encountered: {char}")
 
         index += 1
+
+    while operator_stack:
+        execute_operation(operand_stack, operator_stack)
 
     if not operand_stack:
         raise InsufficientOperandsError("Insufficient operands in the expression")
@@ -89,7 +267,8 @@ def handle_expression(expression: str):
     """
     try:
         print(evaluate_expression(expression))
-    except (InsufficientOperandsError, InvalidNumberFormatError, UnknownCharacterError) as e:
+    except (InsufficientOperandsError, InvalidNumberFormatError, UnknownCharacterError, InsufficientOperatorsError,
+            InvalidUseOfOperatorError) as e:
         print(f"{e.__class__.__name__}: {e}")
 
 
